07aa32187ee974c8c873a564ad94461b4a0530ed
EmanueleRangel <emanuele.rangel52@gmail.com>
2022-06-16 16:25:09 -0300

    add get all clients test

--- Alura.ByteBank.Dados/Migrations/ByteBankContextoModelSnapshot.cs
+++ Alura.ByteBank.Dados/Migrations/ByteBankContextoModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Alura.ByteBank.Dados.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 64)
-                .HasAnnotation("ProductVersion", "5.0.11");
+                .HasAnnotation("ProductVersion", "5.0.12");
 
             modelBuilder.Entity("Alura.ByteBank.Dominio.Entidades.Agencia", b =>
                 {
